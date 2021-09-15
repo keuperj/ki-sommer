e309d7fa0906b367d7c6728ffd8518523db4bdf6
Janis Keuper <keuper@itwm.fhg.de>
2021-09-15 20:32:33 +0200

    rename

--- DeepLearning/HsoMagmaClassification/download_dataset.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir data
-cd data
-wget https://bwsyncandshare.kit.edu/dl/fi8gfej4CYqrkoRujNqqc3BV/magmaDataSet.zip
-unzip magmaDataSet.zip
-rm magmaDataSet.zip
--- DeepLearning/README.md
+++
@@ -1,5 +0,0 @@
-# 2021SummerSchool
-Material on deep learning for the 2021 Summer School at Offenburg University
-
-
-
