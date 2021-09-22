4c913baa45f91e16750b301750d7ae396470c039
pabair <pabair@users.noreply.github.com>
2021-09-22 15:12:24 +0200

    fixed bug

--- Day_3_AppliedML/deploy/server.py
+++ Day_3_AppliedML/deploy/server.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
     basket = request.json['basket']
-    zipCode = request.json['zipCode']
+    customerType = request.json['customerType']
     totalAmount = request.json['totalAmount']
-    p = probability(basket, zipCode, totalAmount)
+    p = probability(basket, customerType, totalAmount)
     return jsonify({'probability': p}), 201
 
-def probability(basket, zipCode, totalAmount):
-    print("Processing request: {},{},{}".format(basket, zipCode, totalAmount))
+def probability(basket, customerType, totalAmount):
+    print("Processing request: {},{},{}".format(basket, customerType, totalAmount))
     return 0.0
 
 if __name__ == "__main__":
